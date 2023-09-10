999a9195a2d7500b6abdb13c3d004f06d37665f5
pablopissoni <pablopissoni@gmail.com>
2023-09-10 11:24:01 -0300

    ajustes en Card

--- Client/src/components/Card.jsx
+++ Client/src/components/Card.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
-
-export const Card = ({name, fecha, puntaje, poster}) => {
-
-
+export const Card = ({ name, fecha, puntaje, poster }) => {
   return (
-    <div className='h-[200px] w-[150px] bg-fuchsia-400'>
-      <div>
-        <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt="" />
+    <div className="h-auto w-[150px] bg-fuchsia-200 rounded-t-2xl ">
+      <div >
+        <img className="rounded-t-2xl" src={`https://image.tmdb.org/t/p/w500${poster}`} alt="" />
       </div>
 
-      <div>
-        <p>{name}</p>
+      <div className="flex flex-col items-center pt-2">
+        <p className="">{name}</p>
         <p>{fecha}</p>
         <p>{puntaje}</p>
       </div>
     </div>
-  )
-}
+  );
+};
